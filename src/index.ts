ca1ba65e148c58461b2bf71e45a8a9ef73db35dd
Brad Garropy <bradgarropy@gmail.com>
2024-06-13 20:53:41 -0500

    use environment variable for cloud name.

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import {v2 as cloudinary} from "cloudinary"
 import {logTags} from "~/src/log"
 
 cloudinary.config({
-    cloud_name: "bradgarropy",
+    cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.API_SECRET,
 })
